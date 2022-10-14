c6e742da71b25ad93156bee5baef60c72b4b650b
Yehor Spyrydonov <extremecode0xff@gmail.com>
2022-10-14 15:34:24 +0300

    React Grid add table

--- src/components/ReactGrid.jsx
+++ src/components/ReactGrid.jsx
@@ -1,47 +1,109 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 
-import { Paper } from '@mui/material';
+import { Paper, Tooltip } from '@mui/material';
 import {
   Grid,
   Table,
   TableHeaderRow,
+  PagingPanel,
+  TableFilterRow,
+  TableColumnResizing,
+  SearchPanel,
+  Toolbar,
 } from '@devexpress/dx-react-grid-material-ui';
-import { SortingState, IntegratedSorting } from '@devexpress/dx-react-grid';
+import {
+  FilteringState,
+  IntegratedFiltering,
+  SortingState,
+  IntegratedSorting,
+  IntegratedPaging,
+  PagingState,
+  DataTypeProvider,
+  SearchState,
+} from '@devexpress/dx-react-grid';
 
 import { MockApi } from '../api/table';
 
+const TooltipFormatter = ({ row: { birthday }, value }) => (
+  <Tooltip title={<span>{`birth date: ${birthday}`}</span>}>
+    <span>{value}</span>
+  </Tooltip>
+);
+
+const CellTooltip = (props) => (
+  <DataTypeProvider
+    for={['age']}
+    formatterComponent={TooltipFormatter}
+    {...props}
+  />
+);
+
 export const ReactGrid = () => {
   const [users, setUsers] = useState([]);
-  const [table, setTable] = useState({ columns: [], rows: [] });
+  const [pageSizes] = useState([5, 10, 15]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pageSize, setPageSize] = useState(5);
+  const [filters, setFilters] = useState([]);
+  const [searchValue, setSearchState] = useState('');
+  const [columnWidths] = useState([
+    { columnName: 'id', width: '15%' },
+    { columnName: 'name', width: '35%' },
+    { columnName: 'surname', width: '35%' },
+    { columnName: 'age', width: '15%' },
+  ]);
   const [sorting, setSorting] = useState([
     { columnName: 'id', direction: 'asc' },
   ]);
 
+  const getAge = (birthDate) =>
+    Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e10);
+
   useEffect(() => {
     MockApi.getUsers().then(({ data }) => {
-      setUsers(Object.values(data));
+      const formatData = Object.values(data).map((user) => {
+        if (user['birthday']) user['age'] = getAge(user['birthday']);
+        return user;
+      });
+      setUsers(Object.values(formatData));
     });
   }, []);
 
-  useEffect(() => {
-    if (users.length) {
-      const columns = Object.entries(users[0]).reduce((acc, [key, value]) => {
-        const capitalize = key.charAt(0).toUpperCase() + key.slice(1);
-        acc.push({ name: key, title: capitalize });
-        return acc;
-      }, []);
-
-      setTable({ columns, rows: users });
-    }
-  }, [users]);
+  const configColumns = useMemo(
+    () => [
+      { name: 'id', title: 'Id' },
+      { name: 'name', title: 'Name' },
+      { name: 'surname', title: 'Surname' },
+      { name: 'age', title: 'Age' },
+    ],
+    []
+  );
 
   return (
     <Paper>
-      <Grid rows={table.rows} columns={table.columns}>
+      <Grid rows={users} columns={configColumns}>
+        <FilteringState filters={filters} onFiltersChange={setFilters} />
+        <SearchState value={searchValue} onValueChange={setSearchState} />
         <SortingState sorting={sorting} onSortingChange={setSorting} />
+        <PagingState
+          currentPage={currentPage}
+          onCurrentPageChange={setCurrentPage}
+          pageSize={pageSize}
+          onPageSizeChange={setPageSize}
+        />
+        <IntegratedFiltering />
         <IntegratedSorting />
+        <IntegratedPaging />
+        <CellTooltip columns={configColumns} />
         <Table />
+        <TableColumnResizing
+          defaultColumnWidths={columnWidths}
+          resizingMode={'nextColumn'}
+        />
         <TableHeaderRow showSortingControls />
+        <PagingPanel pageSizes={pageSizes} />
+        <TableFilterRow />
+        <Toolbar />
+        <SearchPanel />
       </Grid>
     </Paper>
   );
